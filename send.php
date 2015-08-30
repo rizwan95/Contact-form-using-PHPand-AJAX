8333b950abd8c08b1189dccc00703f19d6c4ee89
Rizwan Ahmed <rizwan.ahmed95@gmail.com>
2015-08-30 16:04:42 +0530

    HTML, PHP and AJAX scripts uploaded

---
+++ send.php
@@ -0,0 +1,26 @@
+<?php
+
+// Here we get all the information from the fields sent over by the form.
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+	$to = 'youremail@domain.com';
+	$subject = 'the subject';
+	$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
+	$headers = 'From: youremail@domain.com' . "\r\n";
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
+    mail($to, $subject, $message, $headers); //This method sends the mail.
+	echo "Your email was sent!"; // success message
+}else{
+	echo "Invalid Email, please provide an correct email.";
+}
+	
+
+	
+	
+	
+
+
+?>
